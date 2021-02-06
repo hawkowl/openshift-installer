a1c1fa57b20af10c4230c48ff858ae1615961bb9
Jim Minter <jminter@redhat.com>
2021-02-06 10:56:41 -0600

    HACK: update MCS to use IP on ARO

--- pkg/asset/ignition/machine/master_ignition_customizations.go
+++ pkg/asset/ignition/machine/master_ignition_customizations.go
@@ -11,6 +11,7 @@ import (
 	"github.com/openshift/installer/pkg/asset"
 	"github.com/openshift/installer/pkg/asset/ignition"
 	"github.com/openshift/installer/pkg/asset/installconfig"
+	"github.com/openshift/installer/pkg/asset/templates/content/bootkube"
 	"github.com/openshift/installer/pkg/asset/tls"
 )
 
@@ -33,6 +34,7 @@ func (a *MasterIgnitionCustomizations) Dependencies() []asset.Asset {
 		&installconfig.InstallConfig{},
 		&tls.RootCA{},
 		&Master{},
+		&bootkube.ARODNSConfig{},
 	}
 }
 
@@ -41,9 +43,10 @@ func (a *MasterIgnitionCustomizations) Generate(dependencies asset.Parents) erro
 	installConfig := &installconfig.InstallConfig{}
 	rootCA := &tls.RootCA{}
 	master := &Master{}
-	dependencies.Get(installConfig, rootCA, master)
+	aroDNSConfig := &bootkube.ARODNSConfig{}
+	dependencies.Get(installConfig, rootCA, master, aroDNSConfig)
 
-	defaultPointerIgnition := pointerIgnitionConfig(installConfig.Config, rootCA.Cert(), "master")
+	defaultPointerIgnition := pointerIgnitionConfig(installConfig.Config, aroDNSConfig, rootCA.Cert(), "master")
 	savedPointerIgnition := master.Config
 
 	savedPointerIgnitionJSON, err := ignition.Marshal(savedPointerIgnition)
