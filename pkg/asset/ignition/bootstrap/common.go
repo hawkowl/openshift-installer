e572b0727cc061a24abdd6c15cc756661d01d15f
Ayato Tokubi <atokubi@redhat.com>
2024-03-04 15:47:15 +0100

    CARRY: Update ARO dnsmasq configuration to match current ARO operator (#24)
    * Update ARO dnsmasq configuration to match current ARO operator
    
    Signed-off-by: Marc Sluiter <msluiter@redhat.com>

--- pkg/asset/ignition/bootstrap/common.go
+++ pkg/asset/ignition/bootstrap/common.go
@@ -250,7 +250,7 @@ func (a *Common) generateConfig(dependencies asset.Parents, templateData *bootst
 		a.Config.Storage.Files = append(a.Config.Storage.Files, hostnameFile)
 	}
 
-	dnsmasqIgnConfig, err := dnsmasq.Ignition3Config(installConfig.Config.ClusterDomain(), aroDNSConfig.APIIntIP, aroDNSConfig.IngressIP, aroDNSConfig.GatewayDomains, aroDNSConfig.GatewayPrivateEndpointIP)
+	dnsmasqIgnConfig, err := dnsmasq.Ignition3Config(installConfig.Config.ClusterDomain(), aroDNSConfig.APIIntIP, aroDNSConfig.IngressIP, aroDNSConfig.GatewayDomains, aroDNSConfig.GatewayPrivateEndpointIP, true)
 	if err != nil {
 		return err
 	}
